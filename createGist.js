0c4bdae0a97b2dda7db823c044392bf2ddcab977
Ishita Nandi <irene.pts@gmail.com>
2017-11-02 14:25:57 +0530

    used variables in evaluate function

--- createGist.js
+++ createGist.js
@@ -1,17 +1,28 @@
+#!/usr/bin/env phantomjs
 "use strict";
+
 var page = require('webpage').create();
-page.open('https://github.com/login', function(status) {
+page.open('https://github.com/login', function() {
+  var system = require('system');
+  var env = system.env;
+  var username, password;
+  username = env["Login"];
+  password = env["Password"];
+  console.log("inside " + password);
 
   page.onLoadFinished = function() {
     page.render("after_submit.png");
     phantom.exit();
   };
 
-  page.evaluate(function() {
-    document.querySelector('input[name=login]').value = 'ishitanm';
-    document.querySelector('input[name=password]').value = 'TestISN2017';
-    document.querySelector('input[type=submit]').click();
-  });
-  page.render("output.png");
+  page.evaluate(function(username, password) {
+    //console.log("inside");
 
+    document.querySelector('input[name=login]').value = username;
+    document.querySelector('input[name=password]').value = password;
+    document.querySelector('input[type=submit]').click();
+  }, username, password);
+
+  page.render("output.png");
+  // phantom.exit();
 });
